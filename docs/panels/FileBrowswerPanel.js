fe82279fb701c4c154aab3668451a59091b041a9
Florian Trybel <ftrybel.git@gmail.com>
2026-03-23 09:40:05 +0100

    fixed copy icon

--- docs/panels/FileBrowswerPanel.js
+++ docs/panels/FileBrowswerPanel.js
@@ -35,7 +35,7 @@ export function createRow(obj) {
     </td>
     <td class="ftd">${obj.traj}</td>
     <td class="ftd"><input type="number" min="1" max="${obj.traj}" value="${obj.step}" /></td>
-    <td class="ftd icon copy"><img src="copy.png" alt="⧉"></td>
+    <td class="ftd icon copy">⧉</td>
     <td class="ftd icon delete">×</td>
   `;
 
